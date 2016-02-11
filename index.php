e5cedeeba52ca849e9704ddb9daffd2c81537f0a
Shiwei Huang <shiwei.julia.huang@gmail.com>
2016-02-10 19:19:05 -0500

    Add files for P1

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Shiwei Huang P1 - DWA15 Portfolio</title>
+	<meta charset="utf-8">
+
+	<link rel='stylesheet' href='http://netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css' type='text/css'>
+	<link href="http://netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
+
+	<link href='css/main.css' rel='stylesheet'>
+
+</head>
+<body>
+
+	<div class="container">
+
+		<h1>Shiwei Huang</h1>
+
+		<img src='images/shiweihuang.jpg'>
+
+		<h3>Project 2 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 3 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+		<h3>Project 4 Title</h3>
+		<p>View: [link]</p>
+		<p>Github: [link]</p>
+
+	</div>
+
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body>
+</html>
